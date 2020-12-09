5b6d2fe96a8a899c26259a091fc4a66d51012e22
CarlKarlQarl <karlmatthes@gmail.com>
2020-12-09 15:36:09 -0700

    Set-up AWS CLI script for easier deployment to S3 bucket

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,10 +2,20 @@ import '../fonts/NerkoOne-Regular.ttf';
 import "../styling/LandingPage.css";
 
 const LandingPage = () => {
+
+    const getAnimals = () => {
+        fetch("http://3.128.180.190:8080/animalshelter/animals/")
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }
+
+    getAnimals()
+
     return (
         <div id="container-landing">
             <h1>Fur Haven Animal Shelter</h1>
-            <h2>Bark!</h2>
+            <h2>Meow!</h2>
+            {/* <div>{getAnimals}</div> */}
         </div>
     )
 }
